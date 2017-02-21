b6880d92153a5a62e1db04e8fbcead21c223347b
BigETI <ethem-93@hotmail.de>
2017-02-21 16:51:09 +0100

    Removed unused imports.

--- DiscordETI/src/com/discordeti/core/Commands.java
+++ DiscordETI/src/com/discordeti/core/Commands.java
@@ -2,7 +2,6 @@ package com.discordeti.core;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
--- DiscordETI/src/com/discordeti/core/Servers.java
+++ DiscordETI/src/com/discordeti/core/Servers.java
@@ -1,8 +1,6 @@
 package com.discordeti.core;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map.Entry;
 
 import org.json.JSONObject;
